b5ea3f103da314f61be9fabdc0b7460470df6a9a
Aaron <4709780+2manyvcos@users.noreply.github.com>
2025-08-01 10:37:29 +0000

    add toLowerCase / toUpperCase builtins

---
+++ go/builtins/funcToUpperCase.go
@@ -0,0 +1,32 @@
+package builtins
+
+import (
+	"strings"
+
+	"github.com/jplorg/jpl/go/jpl"
+	"github.com/jplorg/jpl/go/library"
+)
+
+var funcToUpperCase = enclose(func(runtime jpl.JPLRuntime, signal jpl.JPLRuntimeSignal, next jpl.JPLPiper, input any, args ...any) ([]any, error) {
+	t, err := library.Type(input)
+	if err != nil {
+		return nil, err
+	}
+
+	switch t {
+	case jpl.JPLT_STRING:
+		alteredValue, err := library.AlterValue(input, jpl.JPLModifierFunc(func(value any) (any, jpl.JPLError) {
+			return strings.ToUpper(value.(string)), nil
+		}))
+		if err != nil {
+			return nil, err
+		}
+		return next.Pipe(alteredValue)
+	}
+
+	u, err := library.UnwrapValue(input)
+	if err != nil {
+		return nil, err
+	}
+	return nil, library.ThrowAny(library.NewTypeError("%s (%*<100v) cannot be converted to upper case", string(t), u))
+})
